93370b85100df91d040880f1ec59fdb020b2256c
Bilto <mrlaith44@gmail.com>
2023-04-10 22:37:59 +0300

    Update set-hyprYay.sh
    added brave

--- set-hyprYay.sh
+++ set-hyprYay.sh
@@ -28,7 +28,7 @@ if [[ $INST == "Y" || $INST == "y" ]]; then
     check_command_status "yay -S xdg-desktop-portal-hyprland-git"
     yay -R --noconfirm swaylock waybar
     yay -S --noconfirm wlogout
-    yay -S --noconfirm polkit-gnome ffmpeg neovim viewnior rofi pavucontrol thunar starship wl-clipboard wf-recorder swaybg grimblast-git ffmpegthumbnailer tumbler playerctl noise-suppression-for-voice thunar-archive-plugin kitty waybar-hyprland-git swaylock-effects sddm-git pamixer nwg-look-bin nordic-theme papirus-icon-theme dunst otf-sora ttf-nerd-fonts-symbols-common otf-firamono-nerd inter-font ttf-fantasque-nerd noto-fonts noto-fonts-emoji ttf-comfortaa ttf-jetbrains-mono-nerd ttf-icomoon-feather ttf-iosevka-nerd adobe-source-code-pro-fonts ttf-amiri ttf-arabeyes-fonts ttf-qurancomplex-fonts ttf-sil-lateef ttf-sil-scheherazade
+    yay -S --noconfirm polkit-gnome ffmpeg neovim viewnior rofi pavucontrol thunar starship wl-clipboard wf-recorder swaybg grimblast-git ffmpegthumbnailer tumbler playerctl noise-suppression-for-voice thunar-archive-plugin kitty waybar-hyprland-git swaylock-effects sddm-git pamixer nwg-look-bin nordic-theme papirus-icon-theme dunst otf-sora ttf-nerd-fonts-symbols-common otf-firamono-nerd inter-font ttf-fantasque-nerd noto-fonts noto-fonts-emoji ttf-comfortaa ttf-jetbrains-mono-nerd ttf-icomoon-feather ttf-iosevka-nerd adobe-source-code-pro-fonts ttf-amiri ttf-arabeyes-fonts ttf-qurancomplex-fonts ttf-sil-lateef ttf-sil-scheherazade brave-bin
      # Clean out other portals
      
     echo -e "Cleaning out conflicting xdg portals...\n"
