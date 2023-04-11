0cc08cba894c6f57e43ecf30b370b3284a2ae01e
Bilto <mrlaith44@gmail.com>
2023-04-11 19:58:53 +0000

    Update set-hyprYay.sh

--- set-hyprYay.sh
+++ set-hyprYay.sh
@@ -21,12 +21,11 @@ if [[ $NVD == "Y" || $NVD == "y" ]]; then
 else
      yay -S --noconfirm hyprland-bin
 fi
-### Install all of the above pacakges ####
+### Install all of the above packages ####
 read -n1 -rep 'Would you like to install the packages? (y,n)' INST
 if [[ $INST == "Y" || $INST == "y" ]]; then
     yay -S --noconfirm xdg-desktop-portal-hyprland-git
     check_command_status "yay -S xdg-desktop-portal-hyprland-git"
-    yay -R --noconfirm swaylock waybar
     yay -S --noconfirm wlogout
     yay -S --noconfirm polkit-gnome ffmpeg neovim viewnior rofi pavucontrol thunar starship wl-clipboard wf-recorder swaybg grimblast-git ffmpegthumbnailer tumbler playerctl noise-suppression-for-voice thunar-archive-plugin kitty waybar-hyprland-git swaylock-effects sddm-git pamixer nwg-look-bin nordic-theme papirus-icon-theme dunst otf-sora ttf-nerd-fonts-symbols-common otf-firamono-nerd inter-font ttf-fantasque-nerd noto-fonts noto-fonts-emoji ttf-comfortaa ttf-jetbrains-mono-nerd ttf-icomoon-feather ttf-iosevka-nerd adobe-source-code-pro-fonts ttf-amiri ttf-arabeyes-fonts ttf-qurancomplex-fonts ttf-sil-lateef ttf-sil-scheherazade brave-bin
      # Clean out other portals
