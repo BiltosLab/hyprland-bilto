29a1332f30eefd712ac54d89dfbdacc7c6493334
Laith Bilto <mrlaith44@gmail.com>
2023-12-28 05:23:31 +0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+yay -S hyprland eww-wayland ttf-ubuntu-nerd socat jq acpi inotify-tools\
+  bluez pavucontrol brightnessctl playerctl nm-connection-editor imagemagick \
+  gjs gnome-bluetooth-3.0 upower networkmanager gtk3 \
+  wl-gammactl wlsunset wl-clipboard hyprpicker hyprshot blueberry \
+  polkit-gnome
+
+git clone https://github.com/Aylur/dotfiles.git
+
+
+cp -r dotfiles/.config/eww ~/.config/eww
+cp -r dotfiles/.config/hypr ~/.config/hypr
+
+mv ~/.config/hypr/_hyprland.conf ~/.config/hypr/hyprland.conf
+
